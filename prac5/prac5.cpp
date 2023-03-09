5088630dfe08488d2bf391c27e94237eda0ad331
ashen00ne <kaplunov2304@gmail.com>
2023-03-09 13:11:38 +0300

    корректировка сортировки

--- prac5/prac5.cpp
+++ prac5/prac5.cpp
@@ -140,6 +140,7 @@ void insertsort(float* arr, string* FN, int N) {
 	for (int i = 1; i < N; i++) {
 		for (int j = i - 1; j >= 0; j--) if (arr[i] > arr[j]) {
 			swap(arr[i], arr[j]);
+			swap(FN[i], FN[j]);
 			i -= 1;
 		}
 	}
